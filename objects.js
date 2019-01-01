d2058b87d34fa5c9d50db4eef78d90bb0d641528
miao shan <minnieshan@gmail.com>
2019-01-01 13:02:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,9 +6,10 @@ function updatePlaylist(playlist, artist, songTitle){
 
   playlist[artist]=songTitle;
   return playlist;
-  
+
 }
 function removeFromPlaylist(playlist, artistName){
 
-  
+delete playlist[artist];
+ return playlist;
 }
